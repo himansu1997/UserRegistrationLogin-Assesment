668c753c7ca19a5efc5c2bad8244626c28af7e73
Himansu Sethi <hsekhar29@gmail.com>
2021-09-09 11:16:47 +0530

    added product model

--- apps/register/models.py
+++ apps/register/models.py
@@ -28,3 +28,16 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
     objects = UserManager()
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=250)
+    product_number = models.CharField(max_length=100,unique=True)
+    brand = models.CharField(max_length=250, blank=True)
+    description = models.CharField(max_length=300,blank=True,null=True)
+    sale_price = models.DecimalField(max_digits=15, decimal_places=2, default=0.0)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now_add=True)
+    product_category = models.CharField(max_length=250, blank=True) 
+
+    def __str__(self):
+        return "%s"%self.product_number
